88c9f96105bd2bfea78cfc5b4b9564e7a237bbbc
Nairaalmelegy <nairamohamed384@yahoo.com>
2023-12-03 08:07:05 -0800

    0x01

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0 or 0:
+if number > 0 or number == 0:
     Last_dig = number % 10
 elif number < 0:
     Last_dig = number % -10

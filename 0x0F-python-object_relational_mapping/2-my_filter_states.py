f9b5a3fecd596b8b90fbc2e96b2e80dad4f863a4
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-03-16 13:57:31 +0200

    as argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+import MySQLdb
+from sys import argv
 
-    if len(sys.argv) != 5:
-        sys.exit('Use: 1-filter_states.py <mysql username> <mysql password>'
-                 ' <database name> <state name searched>')
-        conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                               passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY "
-                    "'{}' ORDER BY id ASC".format(sys.argv[4]))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
